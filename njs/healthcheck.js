5e859b9c4f93a3c633454f59b920729176d39dfe
Nick Schultz <nschultz@nvisia.com>
2022-02-21 00:59:41 -0600

    update health check to check all services

---
+++ njs/healthcheck.js
@@ -0,0 +1,14 @@
+async function check(r) {
+  check_all(r, ['/database_health', '/export_health', '/merge_health', '/projection_health']);
+}
+
+async function check_all(r, subs) {
+  let results = await Promise.all(subs.map(uri => r.subrequest(uri)));
+  let response = results.map(reply => ({ 
+    uri:  reply.uri, 
+    code: reply.status
+  }));
+  r.return(Math.max(response.map(res => res.code)), JSON.stringify(response));
+}
+
+export default {check};
