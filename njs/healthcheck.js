b5801df04bed29b334e4df9dfacc0709a92bccb5
Nick Schultz <nschultz@nvisia.com>
2022-02-22 23:55:04 -0600

    adjust the return

--- njs/healthcheck.js
+++ njs/healthcheck.js
@@ -8,7 +8,7 @@ async function check_all(r, subs) {
     uri:  reply.uri, 
     code: reply.status
   }));
-  r.return(200, response.map(res => res.code));
+  r.return(200, response.map((res) => res.code));
 }
 
 export default {check};
