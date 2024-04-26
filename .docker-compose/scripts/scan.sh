2e8a67b1138b8f2bd09882a7a4638d898ebb240c
Nick Schultz <nsschultz@gmail.com>
2024-04-26 21:58:57 +0000

    fix sonarqube options

--- .docker-compose/scripts/scan.sh
+++ .docker-compose/scripts/scan.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 rm -rf coverage-results/
 VERSION=$(cat version.txt)
-dotnet build
 dotnet sonarscanner begin \
   /o:"nsschultz" \
   /k:nsschultz_fantasy-baseball-player \
   /n:fantasy-baseball-player \
   /d:sonar.token="$SONAR_TOKEN" \
   /v:$VERSION \
-  /d:"sonar.cs.opencover.reportsPaths=coverage-results/coverage.opencover.xml" \
+  /d:sonar.cs.opencover.reportsPaths=coverage-results/coverage.opencover.xml \
+  /d:sonar.dotnet.excludeTestProjects=true \
   $([ -n "Database/Migrations/*.cs" ] && echo "/d:sonar.coverage.exclusions=\"Database/Migrations/*.cs\"") \
   $([ -n "Database/Migrations/*.cs" ] && echo "/d:sonar.exclusions=\"Database/Migrations/*.cs\"") \
   /d:sonar.host.url="https://sonarcloud.io"
-dotnet test --no-build \
+dotnet test \
   "/p:CollectCoverage=true" \
   "/p:CoverletOutput=../coverage-results/" \
   "/p:CoverletOutputFormat=\"json,opencover\"" \
