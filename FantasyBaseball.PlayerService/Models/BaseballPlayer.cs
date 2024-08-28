b532405f19e8d6d827d55d8f143c56cc918fecef
Nick Schultz <nsschultz@gmail.com>
2024-08-28 06:58:42 +0000

    add json attributes

--- FantasyBaseball.PlayerService/Models/BaseballPlayer.cs
+++ FantasyBaseball.PlayerService/Models/BaseballPlayer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using FantasyBaseball.PlayerService.Models.Enums;
 
 namespace FantasyBaseball.PlayerService.Models
@@ -8,16 +9,16 @@ namespace FantasyBaseball.PlayerService.Models
   public class BaseballPlayer
   {
     /// <summary>The player's ID.</summary>
-    public Guid Id { get; set; }
+    [JsonRequired] public Guid Id { get; set; }
 
     /// <summary>The player's MLB BAM ID.</summary>
-    public int MlbAmId { get; set; }
+    [JsonRequired] public int MlbAmId { get; set; }
 
     /// <summary>The player's BHQ ID.</summary>
-    public int BhqId { get; set; }
+    [Obsolete("Use MlbAmId instead.")] public int BhqId { get; set; }
 
     /// <summary>The player's type (B for Batter or P for Pitcher).</summary>
-    public PlayerType Type { get; set; }
+    [JsonRequired] public PlayerType Type { get; set; }
 
     /// <summary>The player's first name.</summary>
     public string FirstName { get; set; }
@@ -26,22 +27,22 @@ namespace FantasyBaseball.PlayerService.Models
     public string LastName { get; set; }
 
     /// <summary>The player's age.</summary>
-    public int Age { get; set; }
+    [JsonRequired] public int Age { get; set; }
 
     /// <summary>The player's status.</summary>
-    public PlayerStatus Status { get; set; }
+    [JsonRequired] public PlayerStatus Status { get; set; }
 
     /// <summary>The rank of this player in the draft.</summary>
     [Obsolete("Use AvererageDraftPickMin instead.")] public int DraftRank { get; set; }
 
     /// <summary>The average draft position (ADP) for this player.</summary>
-    public double AverageDraftPick { get; set; }
+    [JsonRequired] public double AverageDraftPick { get; set; }
 
     /// <summary>The earliest pick used on this player.</summary>
-    public int AverageDraftPickMin { get; set; }
+    [JsonRequired] public int AverageDraftPickMin { get; set; }
 
     /// <summary>The latest pick used on this player.</summary>
-    public int AverageDraftPickMax { get; set; }
+    [JsonRequired] public int AverageDraftPickMax { get; set; }
 
     /// <summary>The highest pick that was used on this player.</summary>
     [Obsolete("Use AvererageDraftPickMin instead.")] public int HighestPick { get; set; }
@@ -50,16 +51,16 @@ namespace FantasyBaseball.PlayerService.Models
     [Obsolete("Not being used anymore.")] public double DraftedPercentage { get; set; }
 
     /// <summary>The player's projected reliability.</summary>
-    public double Reliability { get; set; }
+    [JsonRequired] public double Reliability { get; set; }
 
     /// <summary>The player's Mayberry Method score.</summary>
-    public int MayberryMethod { get; set; }
+    [JsonRequired] public int MayberryMethod { get; set; }
 
     /// <summary>The player's status for League #1.</summary>
-    public LeagueStatus League1 { get; set; }
+    [JsonRequired] public LeagueStatus League1 { get; set; }
 
     /// <summary>The player's status for League #2.</summary>
-    public LeagueStatus League2 { get; set; }
+    [JsonRequired] public LeagueStatus League2 { get; set; }
 
     /// <summary>Batting Stats (Year to Date, Projected and Combined) for a given player.</summary>
     public List<BattingStats> BattingStats { get; set; } = [];
