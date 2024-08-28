bf6a82cc99bbd8a33bbd40b33859817d28dc70a0
Nick Schultz <nsschultz@gmail.com>
2024-08-28 03:46:20 +0000

    break player action controller into 2 smaller controllers

--- FantasyBaseball.PlayerService.UnitTests/Controllers/V3/PlayerUploadStatsControllerTest.cs
+++ FantasyBaseball.PlayerService.UnitTests/Controllers/V3/PlayerUploadStatsControllerTest.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using FantasyBaseball.PlayerService.Controllers.V3;
 using FantasyBaseball.PlayerService.Exceptions;
 using FantasyBaseball.PlayerService.FileReaders;
-using FantasyBaseball.PlayerService.Models;
 using FantasyBaseball.PlayerService.Models.Enums;
 using FantasyBaseball.PlayerService.Services;
 using Microsoft.AspNetCore.Http;
@@ -15,31 +12,8 @@ using Xunit;
 
 namespace FantasyBaseball.PlayerService.UnitTests.Controllers.V3
 {
-  public class PlayerActionControllerTest
+  public class PlayerUploadStatsControllerTest
   {
-
-    [Fact]
-    public async Task GetFile()
-    {
-      var fileContent = Encoding.ASCII.GetBytes("file-content");
-      var getService = new Mock<IGetPlayerService>();
-      getService.Setup(o => o.GetPlayers()).ReturnsAsync([new()]);
-      var writer = new Mock<ICsvFileWriterService>();
-      writer.Setup(o => o.WriteCsvData(It.Is<List<BaseballPlayer>>(p => p.Count == 1))).Returns(fileContent);
-      var controller = new PlayerActionController(getService.Object, writer.Object, null);
-      var headerDictionary = new HeaderDictionary();
-      var response = new Mock<HttpResponse>();
-      response.SetupGet(r => r.Headers).Returns(headerDictionary);
-      var httpContext = new Mock<HttpContext>();
-      httpContext.SetupGet(a => a.Response).Returns(response.Object);
-      controller.ControllerContext = new ControllerContext() { HttpContext = httpContext.Object };
-      var result = await controller.ExportPlayers();
-      Assert.Equal("players.csv", ((FileContentResult)result).FileDownloadName);
-      Assert.Equal(fileContent, ((FileContentResult)result).FileContents);
-      getService.VerifyAll();
-      writer.VerifyAll();
-    }
-
     [Fact]
     public async Task UploadStatsValidTest()
     {
@@ -47,7 +21,7 @@ namespace FantasyBaseball.PlayerService.UnitTests.Controllers.V3
       mergeService
         .Setup(o => o.MergeStats(It.IsAny<FormFileReader>(), It.Is<PlayerType>(i => i == PlayerType.B), It.Is<StatsType>(i => i == StatsType.YTD)))
         .ReturnsAsync(6);
-      var controller = new PlayerActionController(null, null, mergeService.Object);
+      var controller = new PlayerUploadStatsController(mergeService.Object);
       var request = new Mock<HttpRequest>();
       var httpContext = new Mock<HttpContext>();
       httpContext.SetupGet(a => a.Request).Returns(request.Object);
@@ -59,7 +33,7 @@ namespace FantasyBaseball.PlayerService.UnitTests.Controllers.V3
     public async Task UploadStatInvalidPlayerTest()
     {
       var mergeService = new Mock<IMergeStatsService>();
-      var controller = new PlayerActionController(null, null, mergeService.Object);
+      var controller = new PlayerUploadStatsController(mergeService.Object);
       await Assert.ThrowsAsync<BadRequestException>(() => controller.UploadStats(PlayerType.U, StatsType.PROJ));
     }
 
@@ -67,7 +41,7 @@ namespace FantasyBaseball.PlayerService.UnitTests.Controllers.V3
     public async Task UploadStatsInvalidStatsTest()
     {
       var mergeService = new Mock<IMergeStatsService>();
-      var controller = new PlayerActionController(null, null, mergeService.Object);
+      var controller = new PlayerUploadStatsController(mergeService.Object);
       await Assert.ThrowsAsync<BadRequestException>(() => controller.UploadStats(PlayerType.P, StatsType.UNKN));
     }
 
@@ -79,7 +53,7 @@ namespace FantasyBaseball.PlayerService.UnitTests.Controllers.V3
       mergeService
         .Setup(o => o.MergeStats(It.IsAny<FormFileReader>(), It.Is<PlayerType>(i => i == PlayerType.P), It.Is<StatsType>(i => i == StatsType.YTD)))
         .ThrowsAsync(new Exception("Bad Request"));
-      var controller = new PlayerActionController(null, null, mergeService.Object);
+      var controller = new PlayerUploadStatsController(mergeService.Object);
       var request = new Mock<HttpRequest>();
       var httpContext = new Mock<HttpContext>();
       httpContext.SetupGet(a => a.Request).Returns(request.Object);
--- FantasyBaseball.PlayerService/Controllers/V3/PlayerUploadStatsController.cs
+++ FantasyBaseball.PlayerService/Controllers/V3/PlayerUploadStatsController.cs
@@ -1,48 +1,30 @@
 using System;
-using System.Net.Mime;
 using System.Threading.Tasks;
 using FantasyBaseball.PlayerService.Exceptions;
 using FantasyBaseball.PlayerService.FileReaders;
 using FantasyBaseball.PlayerService.Models.Enums;
 using FantasyBaseball.PlayerService.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FantasyBaseball.PlayerService.Controllers.V3
 {
   /// <summary>Endpoint for taking actions on the player objects.</summary>
-  [Route("api/v2/action")]
-  [Route("api/v3/action")]
+  [Route("api/v2/action/upload/stats")]
+  [Route("api/v3/action/upload/stats")]
   [ApiController]
-  public class PlayerActionController : ControllerBase
+  public class PlayerUploadStatsController : ControllerBase
   {
-    private readonly ICsvFileWriterService _writerService;
-    private readonly IGetPlayerService _getService;
     private readonly IMergeStatsService _mergeService;
 
-    /// <summary>Creates a new instance of the <see cref="PlayerActionController"/> class.</summary>
-    /// <param name="getService">Service for getting players.</param>
-    /// <param name="writerService">The service for writing the CSV file.</param>
+    /// <summary>Creates a new instance of the <see cref="PlayerUploadStatsController"/> class.</summary>
     /// <param name="mergeService">Service for merging the CSV file into the existing data store.</param>
-    public PlayerActionController(IGetPlayerService getService, ICsvFileWriterService writerService, IMergeStatsService mergeService) =>
-      (_getService, _mergeService, _writerService) = (getService, mergeService, writerService);
-
-    /// <summary>Exports the players as a CSV file.</summary>
-    /// <returns>A CSV file containing the players.</returns>
-    [HttpGet("export")]
-    public async Task<IActionResult> ExportPlayers()
-    {
-      var players = await _getService.GetPlayers();
-      var fileContent = _writerService.WriteCsvData(players);
-      Response.Headers.Append("Content-Disposition", new ContentDisposition { FileName = "players.csv", Inline = false }.ToString());
-      return File(fileContent, "application/octet-stream", "players.csv");
-    }
+    public PlayerUploadStatsController(IMergeStatsService mergeService) => (_mergeService) = (mergeService);
 
     /// <summary>Uploads player statistics from a file.</summary>
     /// <param name="player">The type of player.</param>
     /// <param name="stats">The type of statistics.</param>
     /// <returns>The number of records merged.</returns>
-    [HttpPost("upload/stats")]
+    [HttpPost]
     public async Task<int> UploadStats(PlayerType player, StatsType stats)
     {
       try
